af14fb8b22faedd17c06c4b5f655b5aba28f43d1
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-23 17:45:11 +0530

    Student Information

---
+++ variable & data type/1.py
@@ -0,0 +1,14 @@
+#🐍 Variables & Data Types — 10 Questions
+# Student Information
+
+Name = "Vaibhav Rathore"
+age = 21
+Course = "B.tech CSE"
+College = "SRI AUROBINDO INSTITUTE OF TECHNOLOGY"
+cgpa = 6.72
+
+
+print(Name)
+print(age)
+print(cgpa)
+print(College)
