0597cac0f1107fdab1bc0f01e7a9f684a2bad745
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-23 16:44:42 +0530

    Data type int,string,float,multiple variable,Type chacking, Type casting

---
+++ Data Type .py
@@ -0,0 +1,55 @@
+# Data type - int-> integer , String
+
+Name = "vaibhav Rathore"
+Age = 21
+Marks = 77
+City = "Indore"
+
+print(Name)
+print(Age)
+print(City)
+print(Marks)
+
+# Float
+price = 99.8
+Height = 5.10
+
+print(price)
+print(Height)
+
+#Boolean
+is_student = True
+is_married = False
+
+print(type(is_student))
+
+#Multiple variable
+name , age , city = "om" , 21, "indore"
+print(age)
+print(name)
+print(city)
+
+#one more example
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+
+#Type checking
+x = 10
+student = "Rathore"
+print(type(x))
+print(type(student))
+
+#Type casting
+x = "100" # int > string cast
+y = 200
+a = 33
+b = float(a) # int convert into float
+
+print(type(x))
+print(type(y))
+print(b)
+
