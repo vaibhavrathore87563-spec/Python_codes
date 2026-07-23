c63cfcd2783941c1b08f3d28fd3ba0345370bb7f
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-23 19:40:02 +0530

    sum(),Average() of subjects

---
+++ variable & data type/07_student_marks.py
@@ -0,0 +1,11 @@
+#Store five subjects marks in variable
+English = 57
+Hindi = 75
+Maths = 69
+Physics = 78
+Chemistry = 82
+
+total = Maths , English, Hindi, Chemistry, Physics/5
+
+print("Total Marks:=",total)
+print("average marks:=",total)
