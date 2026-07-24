fa64008f9266e2522d07f0d5b17278100c410b18
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-24 16:40:38 +0530

    💬 Add rectangle area and perimeter calculator

---
+++ Arithmetic  operator/rectangle_calculator.py
@@ -0,0 +1,10 @@
+#Rectangle_calculator
+
+length = 12
+breadth = 10
+
+area = length * breadth
+peramiter = 2 *(length + breadth)
+
+print("Area of Rectangle", area)
+print("peramiter of Rectangle:",peramiter)
