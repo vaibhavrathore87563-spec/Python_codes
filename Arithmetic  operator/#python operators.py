6ea6254cb7359582e57d9b4707b6cf2260d48122
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-24 16:01:16 +0530

    Add arithmetic operators practice

---
+++ Arithmetic  operator/#python operators.py
@@ -0,0 +1,11 @@
+#Arthmetic operators
+
+a = 20
+b = 6
+print("Addition:" ,a+b)
+print("Substraction:" ,a-b)
+print("Multiplication:" ,a*b)
+print("Division:" ,a/b)
+print("Modulus:" ,a%b)
+print("Power:" ,a**b)
+print("Floor Division:" ,a//b)
