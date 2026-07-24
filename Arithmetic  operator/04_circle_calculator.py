86b4a28c252b666981609c5ce6a0218d25d1a30e
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-24 18:36:28 +0530

    Add simple interest calculator

---
+++ Arithmetic  operator/04_circle_calculator.py
@@ -0,0 +1,8 @@
+radius = 5
+pi = 3.14
+
+area = pi * radius * radius
+circumference = 2 * pi * radius
+
+print("Area:" , area)
+print("Circumference:", circumference)
---
+++ Arithmetic  operator/1.py
@@ -0,0 +1,9 @@
+principal = 10000
+rate = 5
+time = 2
+
+simple_intrest = (principal * rate * time)/100
+Total_Amount = principal + simple_intrest
+
+print("Simple_Interest:=",simple_intrest)
+print("Total_Amount:=",Total_Amount)
