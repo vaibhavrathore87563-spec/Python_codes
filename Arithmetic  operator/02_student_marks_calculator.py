3f760314bc2e5a2c9677b703ca95b42118a82385
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-24 16:31:45 +0530

    Add student marks calculator

---
+++ Arithmetic  operator/02_student_marks_calculator.py
@@ -0,0 +1,14 @@
+#subject marks calculator
+English = 68 
+Hindi = 76 
+Maths = 72 
+Science = 62 
+sanskrit = 91
+
+total = English + Hindi + Maths+Science + sanskrit
+average = total / 5
+percentage = (total / 500) *100
+
+print("Total Marks:=",total)
+print("Average Marks:" , average)
+print("percentage:=" ,percentage, "%")
